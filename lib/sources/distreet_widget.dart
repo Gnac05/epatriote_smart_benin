84b45179078dd2770200829636f43276c35b0bf3
Serge GNACADJA <sergegnac2003@gmail.com>
2021-05-19 19:14:34 +0100

    stateful

---
+++ lib/sources/distreet_widget.dart
@@ -0,0 +1,14 @@
+
+class DistreetWidget extends StatefulWidget {
+  @override
+  _DistreetWidgetState createState() => _DistreetWidgetState();
+}
+
+class _DistreetWidgetState extends State<DistreetWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
